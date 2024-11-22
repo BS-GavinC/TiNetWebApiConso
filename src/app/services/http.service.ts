a5a6de8241668e2288af09986ec90c734cb4b82f
gchaineux <gavin.chaineux@bstorm.be>
2024-11-22 16:29:18 +0100

    versioning

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -4,8 +4,10 @@ import {HttpClient} from '@angular/common/http';
 export abstract class HttpService {
 
   constructor(route : string) {
-    this.route = 'https://localhost:7070/api/' + route;
+    this.route = 'https://localhost:7070/api/' + this.apiVersion + '/' + route;
   }
 
   protected route: string;
+
+  protected apiVersion : string = 'v2'
 }
